49901f03ef35fd208edb177c7b01e68b822cddd3
fkdkskdfjfk <yljw225@naver.com>
2024-12-06 23:54:26 +0900

    배포2

--- src/app/api/books/[id]/route.tsx
+++ src/app/api/books/[id]/route.tsx
@@ -4,7 +4,7 @@ import fs from "fs/promises";
 import path from "path";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const { id } = await params;
+  const { id } = params;
   console.log(`Received ID: ${id}`);
   
   if (!id) {
